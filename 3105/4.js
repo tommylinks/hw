7a812e9688b4b8540f52ba1a93c4320dac2f0d5d
tommylinks <tommylinks1@gmail.com>
2017-05-31 17:04:55 +0300

    fix bugs

--- 3105/4.js
+++ 3105/4.js
@@ -5,66 +5,67 @@
 //   return !isNaN(parseFloat(n)) && isFinite(n);
 // }
 
+
+
+
 // var sum = 0;
 
 // function func(arg) {
 
 // 	if(isNumeric(arg)) {
+
 // 		return sum += arg;
+
 // 	} else {
 // 		return 0;
 // 	}
-
 // }
+
+
+
 
 // task 1 option 1 (using closures)
 
 
-// function isNumeric(n) {
-//   return !isNaN(parseFloat(n)) && isFinite(n);
-// }
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 
 
+var func = function() {
 
+	var sum = 0
+		return function(arg) {
+			if(isNumeric(arg)) {
+					return sum += arg;
 
-// var func = function(arg) {
+			} else { return 0; }
+		};
+		
+};
 
-// 	var sum = 0;
-
-// 	if(isNumeric(arg)) {
-
-// 		return function() {
-// 			return sum += arg;
-// 		}
-
-// 	} else {
-// 		return 0;
-// 	}
-
-// }
-
-// var result = func;
+var result = func();
 
 
 
 // task 2
 
 
-var myObj = {
-	dima: 'Dima',
-	age: 25
-}
+// var myObj = {
+// 	dima: 'Dima',
+// 	age: 25
+// }
 
-var resultFunc = function(obj) {
-	obj.canRun = 'true';
-	console.log(obj);
-}
+// var resultFunc = function(obj) {
+// 	obj.canRun = 'true';
+// 	console.log(obj);
+// }
 
-var func = function(f) {
-	f(myObj);
-}
+// var func = function(f) {
+// 	f(myObj);
+// }
 
-func(resultFunc);
+// func(resultFunc);
 
 
